801c49fb13774b88b8b5991ffa42e9627a952244
Kadeska <19519433+kadeska@users.noreply.github.com>
2023-06-26 16:37:08 -0700

    Created basic user input.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+
+/*variables*/
+
+bool running;
+// user input
+std::string input;
+
 int main(int argc, char const *argv[])
 {
     /* code */
@@ -10,5 +17,28 @@ int main(int argc, char const *argv[])
     std::cout << "first arg passed: ";
     std::cout << argv[1];
     std::cout << "\n";
+
+    if (argc > 2) {
+        std::cout << "Sorry, you gave too many arguments. Please use -help for help.\n";
+        return 1;
+    }
+    input = argv[1];
+    running = true;
+    start();
     return 0;
+}
+
+// Start app 
+void start() {
+    while (running) {
+        std::cin >> input;
+
+        if (input == "-help") {
+            std::cout << "Testing...";
+            return;
+        }
+
+        std::cout << "please specify an argument.\n";
+        return;
+    }
 }
