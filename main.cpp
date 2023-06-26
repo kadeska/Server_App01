f5cce2e490f37e0a5e71273d5789de2db631185b
Joseph <kadeska23@gmail.com>
2023-06-26 12:31:45 -0700

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    std::cout << "hello world!\n";
+    std::cout << "Number of args passed: ";
+    std::cout << argc + " \n";
+    std::cout << "";
+    return 0;
+}
