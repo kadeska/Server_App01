5738b21312ed6d3f8e4dfbc5f118fb0d6c52f643
Joseph <kadeska23@gmail.com>
2023-06-26 12:54:26 -0700

    testing

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main(int argc, char const *argv[])
     /* code */
     std::cout << "hello world!\n";
     std::cout << "Number of args passed: ";
-    std::cout << argc + " \n";
+    std::cout << "" + argc;
+    std::cout << "\n";
     std::cout << "first arg passed: ";
-    std::cout << argv[1] + "\n";
+    std::cout << argv[1];
+    std::cout << "\n";
     return 0;
 }
